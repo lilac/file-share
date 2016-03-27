7537f51109c1381e579f70290329c176032afc7c
James Deng <cnJamesDeng@gmail.com>
2016-03-27 18:26:34 +1100

    Fix asset dependency issues

--- project/Build.scala
+++ project/Build.scala
@@ -1,3 +1,5 @@
+import com.typesafe.sbt.web.Import
+import com.typesafe.sbt.web.SbtWeb
 import play.sbt.PlayScala
 import sbt._
 import play.sbt.Play.autoImport._
@@ -17,7 +19,11 @@ object ApplicationBuild extends Build {
     "org.scalatra.scalate" %% "scalate-core" % "1.7.1",
     "org.squeryl" %% "squeryl" % "0.9.5-6",
     "org.scalaz" %% "scalaz-core" % "6.0.4",
-    "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.2"
+    "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.2",
+
+    "org.webjars" %% "webjars-play" % "2.4.0",
+    "org.webjars" % "bootstrap" % "3.3.6",
+    "org.webjars" % "jquery" % "2.2.2"
   )
 
   val appResolvers = Seq()
@@ -36,10 +42,12 @@ object ApplicationBuild extends Build {
   )
   // lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-  val main = Project(appName, file(".")).enablePlugins(play.PlayScala).settings(
+  val main = Project(appName, file(".")).enablePlugins(play.PlayScala, SbtWeb).settings(
     version := appVersion,
     scalaVersion := "2.10.5",
     libraryDependencies ++= appDependencies,
     watchSources <++= baseDirectory map { path => ((path / "app" / "views") ** "*.jade").get }
   )
+
+  // Import.pipelineStages := Seq(RjsKeys.rjs)
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,6 +4,18 @@ resolvers ++= Seq(
   "Typesafe Snapshots" at "http://repo.typesafe.com/typesafe/snapshots/",
   "Codahale Repo" at "http://repo.codahale.com")
 
+resolvers += Resolver.sonatypeRepo("releases")
+
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.4.2")
 
 addSbtPlugin("org.ensime" % "ensime-sbt-cmd" % "latest.milestone")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-less" % "1.1.0")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-coffeescript" % "1.0.0")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-rjs" % "1.0.7")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-uglify" % "1.0.3")
+
+addSbtPlugin("net.ground5hark.sbt" % "sbt-css-compress" % "0.1.3")
